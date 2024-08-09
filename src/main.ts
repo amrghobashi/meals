484aa034ff42fecb6c6db049150942d09d28d413
amrghobashi <amrghobashi7@gmail.com>
2024-08-09 22:42:04 +0300

    start working with ngrx

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
 import 'zone.js';
+import { provideStore } from '@ngrx/store';
+import { reducers, metaReducers } from './app/reducers';
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient(), provideAnimations()],
+  providers: [provideHttpClient(), provideAnimations(), provideStore(reducers, { metaReducers })],
 });
 
